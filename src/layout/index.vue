477274bc7b148a6c80fec0dad7e61027b6016e88
oliviaAZ <oliviazxj@sina.com>
2023-05-29 15:39:32 +0800

    初始化项目

---
+++ src/layout/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <nav-bar></nav-bar>
+  </div>
+</template>
+<script setup lang="ts">
+import navbar from "./components/navbar.vue";
+</script>
+<style lang="scss" scoped></style>
